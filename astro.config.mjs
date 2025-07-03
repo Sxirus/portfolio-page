8af3c7f75878d6fd28245ebe2e345d0f5e6a0db2
Praktikums PC <jakob.hinze@icloud.com>
2025-07-03 14:36:25 +0200

    finished the course

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
-import preact from "@astrojs/preact"; 
+import preact from "@astrojs/preact";
+
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    site: "https://jakobhinze.netlify.app/",
-    integrations: [preact()]
+  site: "https://jakobhinze.netlify.app/",
+  integrations: [preact()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
