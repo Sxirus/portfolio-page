bcbc6f02bc1e00f8ee3569f6659c24e42975b7d6
Praktikums PC <jakob.hinze@icloud.com>
2025-07-09 15:38:13 +0200

    .

--- .netlify/build/chunks/astro_C_aT6rqn.mjs
+++
@@ -1,3 +0,0 @@
-import 'kleur/colors';
-import './astro/server_Bl5g64wB.mjs';
-import 'clsx';
--- .netlify/build/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_DuSrpQ6A.mjs';
-export { renderers } from '../renderers.mjs';
